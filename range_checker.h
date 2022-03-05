61572c8e159fc0ec5288214b74a744b15bc2fec4
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-03-05 17:40:39 +0530

    Update range_checker.h

--- range_checker.h
+++ range_checker.h
@@ -1,6 +1,9 @@
 #ifndef RANGE_CHECKER_H_
 #define RANGE_CHECKER_H_
 
+#include <stdio.h>
+#include <string.h>
+
 extern char RangeAndReading[DATA_STREAM_SIZE];
 
 void checkNumberOfReadingsInRange(int inputStream[], int rangeStart, int rangeEnd);
