634ac3028663330ad42bf9bc0a9309b184f11ce0
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-03-05 18:21:15 +0530

    Update test_code.cpp

--- test_code.cpp
+++ test_code.cpp
@@ -12,9 +12,9 @@ TEST_CASE("check number of readings in given range from charging session"){
 }
 
 TEST_CASE("check of no readings found in given range from charging session"){
-  	int data[DATA_STREAM_SIZE] = {3,0,-2};
+  	int data[DATA_STREAM_SIZE] = {3,0,-2,10,100};
 	int rangeLow = 4;
-	int rangeHigh = 5;
+	int rangeHigh = 7;
 	checkNumberOfReadingsInRange(data, rangeLow, rangeHigh);
-	REQUIRE(!strncmp(RangeAndReading, "4-5,0", 5));
+	REQUIRE(!strncmp(RangeAndReading, "4-7,0", 5));
 }
