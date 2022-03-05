6b3cbc09b577183b255c1a4c290ad74c87189c5b
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-03-05 12:17:44 +0530

    Update test_code.cpp

--- test_code.cpp
+++ test_code.cpp
@@ -1,4 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
+#include "test/catch.hpp"
 
+TEST_CASE("check number of readings in given range from charging session"){
+  int inputRange[] = {4,5};
+	checkNumberOfReadingsInRange(inputRange);
+	REQUIRE(4-5, 2);
