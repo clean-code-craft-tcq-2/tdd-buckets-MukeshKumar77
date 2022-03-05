5194557e3659f0dc625a9a073d8affc30d501039
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-03-05 17:35:21 +0530

    Update range_checker.c

--- range_checker.c
+++ range_checker.c
@@ -1,6 +1,20 @@
 #include "range_checker.h"
 
-char* checkNumberOfReadingsInRange(int inputStream[], int rangeStart, int rangeEnd)
+char RangeAndReading[DATA_STREAM_SIZE];
+
+void checkNumberOfReadingsInRange(int* inputStream, int rangeStart, int rangeEnd)
 {
-	return "4-5,2";
+	int i, numberOfReadings = 0;
+	for(i = 0; i < DATA_STREAM_SIZE; i++)
+	{
+		if((inputStream[i] >= rangeStart) && (inputStream[i] <= rangeEnd))
+		{
+			numberOfReadings++;
+		}
+	}
+
+	char tempRangeAndReading[DATA_STREAM_SIZE];
+	sprintf(tempRangeAndReading, "%d-%d,%d",rangeStart, rangeEnd, numberOfReadings);
+
+	strcpy(RangeAndReading, tempRangeAndReading);
 }
